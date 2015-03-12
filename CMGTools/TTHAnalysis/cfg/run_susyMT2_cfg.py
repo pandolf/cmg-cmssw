8c0fce3763a19f08a174d99c9aaf16fc2feb070c
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2015-03-12 13:29:40 +0100

    added randomCone=true

--- CMGTools/TTHAnalysis/cfg/run_susyMT2_cfg.py
+++ CMGTools/TTHAnalysis/cfg/run_susyMT2_cfg.py
@@ -56,6 +56,7 @@ tauAna.tauAntiElectronID = "againstElectronLoose"
 # Photon
 photonAna.etaCentral = 2.5
 photonAna.gammaID = "PhotonCutBasedIDLoose_CSA14"
+photonAna.do_randomCone = True
 
 # Isolated Track
 isoTrackAna.setOff=False
