dc1928c265887ffba7708e6c80d5f85bd02907f8
mschoene <mschoene@cern.ch>
2018-01-22 13:17:53 +0100

    fixed 2017 photon id iso

--- PhysicsTools/Heppy/python/physicsobjects/Photon.py
+++ PhysicsTools/Heppy/python/physicsobjects/Photon.py
@@ -216,6 +216,11 @@ class Photon(PhysicsObject ):
                  passPhotonIso = False
              if self.calScaledIsoValueLin(*self.CutBasedIDWP(name)["phoIso"][idForBarrel]) < self.photonIso(isocorr):
                  passPhotonIso = False
+        elif "POG_Spring17" in name:
+             if self.calScaledIsoValueQuadr(*self.CutBasedIDWP(name)["neuHadIso"][idForBarrel]) < self.neutralHadronIso(isocorr):
+                 passPhotonIso = False
+             if self.calScaledIsoValueLin(*self.CutBasedIDWP(name)["phoIso"][idForBarrel]) < self.photonIso(isocorr):
+                 passPhotonIso = False
         else:
             if self.calScaledIsoValueLin(*self.CutBasedIDWP(name)["neuHadIso"][idForBarrel]) < self.neutralHadronIso(isocorr):
                 passPhotonIso = False
